d87b9387acd8e940e95a73b4b03c5f7a82cd28f1
Jonteb02 <jbirgersson@me.com>
2022-03-07 20:45:03 +0100

    gg

--- public/js/index.js
+++ public/js/index.js
@@ -424,7 +424,7 @@ function lose(){
 function gameOver(){
   dynDrawScore();
   playing = false;
-  scoreForm.style.display = "contents";
+  scoreForm.style.visibility="visible"
   canvas.style.background = "#333";
   if(player.lives <= 0){
     player.lives = 0;
@@ -448,6 +448,7 @@ function submitHighScore(){
   let url ="https://jonathan-birgersson-backend.herokuapp.com/registerscore?user=" + playerName + "&score=" + playerScore;
   xhr.open("GET", url);
   xhr.send();
+  scoreForm.style.visibility="visible"
 }
 
 function update(){
